ba8efeef154fdf8f5730699a7f4aaa46790a2103
edo <edouardlicn@gmail.com>
2026-08-07 13:49:59 +0800

    20260807update1

--- cimf_django/urls.py
+++ cimf_django/urls.py
@@ -2,6 +2,8 @@
 URL configuration for cimf_django project.
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -19,3 +21,7 @@ urlpatterns = [
     path("importexport/", include((importexport_urls, "importexport"), namespace="importexport")),
     path("api/v1/", include("core.api_urls", namespace="api")),
 ]
+
+# 媒体文件静态服务（WhatsApp 媒体发送依赖 WABridge 拉取 /media/ 下的文件）
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
